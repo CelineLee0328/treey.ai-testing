71e6756b69191e7085f3713d55ae1638d17060f5
PutoDoTulicreme <joao.castela.simao@gmail.com>
2025-11-19 12:22:36 +0100

    Header added. Using tailwindcss, basically the css is inside the class names. No need to work on two files.

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="ComingSoon">
-      <div className="Content">
-        <div className="Title">Coming Soon</div>
-        <div className="Subtitle">treey.ai</div>
-      </div>
-    </div>
+   <main className="min-h-screen bg-gradient-to-br from-green-400 to-emerald-400 flex flex-col">
+      <header className="flex justify-between items-center px-8 py-4 bg-green-600 backdrop-blur-sm shadow-md">
+        <div className="text-xl font-bold">🌳 treey.ai</div>
+        <div className="text-lg font-medium">Coming soon...</div>
+      </header>
+
+      <section className="flex flex-1 items-center justify-center">
+        <h1 className="text-5xl font">Hello there!</h1>
+      </section>
+
+
+   </main>
   );
 }
