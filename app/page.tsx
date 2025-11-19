799692a1199dbfcf9ca9c6f77dc8e812367dd5e8
PutoDoTulicreme <joao.castela.simao@gmail.com>
2025-11-19 13:38:35 +0100

    Footernote added!

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <main className="min-h-screen bg-gradient-to-br from-green-400 to-emerald-400 flex flex-col">
-      <header className="flex justify-between items-center px-8 py-4 bg-green-600 backdrop-blur-sm shadow-md">
+    <main className="min-h-screen bg-gradient-to-br from-green-400 to-emerald-400 flex flex-col">
+    
+      <header className="flex justify-between items-center px-8 py-4 bg-green-600 shadow-md">
         <div className="text-xl font-bold">🌳 treey.ai</div>
-        <div className="text-lg font-medium">Coming soon...</div>
       </header>
 
       <section className="flex flex-1 items-center justify-center">
-        <h1 className="text-5xl font">Hello there!</h1>
+        <h1 className="text-5xl font-bold">Hello there! <br></br>Its about to start...</h1>
       </section>
 
+      <footer className="bg-green-600 text-white shadow-md px-8 py-4 flex flex-col items-center gap-1">
+        <p className="font-semibold">Our Team</p>
+        <p>João – joao@example.com | Celine – celine@example.com | Sylvia – sylvia@example.com</p>
+      </footer>
 
-   </main>
+    </main>
   );
 }
